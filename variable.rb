287bcb4b7528c27f905c9c11d141b034feb8fa6e
Israel Canessa <israellcanessa@gmail.com>
2020-05-02 04:38:36 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,1 +1,8 @@
 greeting = "Hello World"
+name = "Fernando Israel Garcia Camacho"
+puts 'My name is '+name+''
+composer = 'Choplin'
+puts 'okay! nice to meet you! '+name+''
+puts 'did you know '+composer+' was one the greatest ever!?'
+composer = 'mozart'
+puts 'however I prefer '+composer+'.'
